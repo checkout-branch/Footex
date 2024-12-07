e9a8c09cd73b554025a938352bd2e3f1a73ef6bf
Raseel <raseelp321@gmail.com>
2024-12-07 10:27:49 +0530

    Made  A navebar

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import styles from "./Header.module.scss";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Header = () => {
-  return (
-    <div className={styles.header}>Header</div>
+  return (<>
+    <header className={styles.header}>
+      <div className={styles.logo}>Footex</div>
+      <div className={styles.navRight}>
+        <button className={styles.topButtons}><a href="https://www.instagram.com/footex_/#" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
+          <FaInstagram className={styles.icon} />
+        </a></button>
+        <button className={styles.topButtons}><a href="https://api.whatsapp.com/send/?phone=919656060874&text&type=phone_number&app_absent=0" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp className={styles.icon} />
+        </a></button>
+        <button className={styles.topButtons}><RxHamburgerMenu className={styles.icon} /></button>
+      </div>
+    </header>
+  </>
   )
 }
 
-export default Header
+export default Header;
