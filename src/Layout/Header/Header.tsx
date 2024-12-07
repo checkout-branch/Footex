84a6afa3d656a16b8f8a146f178fa91fff740196
Raseel <raseelp321@gmail.com>
2024-12-07 11:03:58 +0530

    Added logo to navBar

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styles from "./Header.module.scss";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
+import logoImage from "../../assets/logo.png";
 const Header = () => {
   return (<>
     <header className={styles.header}>
-      <div className={styles.logo}>Footex</div>
+      <div className={styles.logo}> <img src={logoImage} alt="Footex Logo" className={styles.logoImage} /></div>
       <div className={styles.navRight}>
         <button className={styles.topButtons}><a href="https://www.instagram.com/footex_/#" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
           <FaInstagram className={styles.icon} />
